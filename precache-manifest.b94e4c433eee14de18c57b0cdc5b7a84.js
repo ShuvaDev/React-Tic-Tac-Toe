dd0a7fc3cf4d6d429422f1f6988b1ddaca50e85f
ShuvaDev <s.r.shuvadeb@gmail.com>
2020-05-25 23:23:37 +0600

    Updates

---
+++ precache-manifest.b94e4c433eee14de18c57b0cdc5b7a84.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be416e4df1ff9c54c6e86c309b40d7b9",
+    "url": "/React-Tic-Tac-Toe/index.html"
+  },
+  {
+    "revision": "18e4aec581ee316befee",
+    "url": "/React-Tic-Tac-Toe/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "ef84f08eee751d34daa3",
+    "url": "/React-Tic-Tac-Toe/static/css/main.113f19db.chunk.css"
+  },
+  {
+    "revision": "18e4aec581ee316befee",
+    "url": "/React-Tic-Tac-Toe/static/js/2.4e633016.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Tic-Tac-Toe/static/js/2.4e633016.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef84f08eee751d34daa3",
+    "url": "/React-Tic-Tac-Toe/static/js/main.5ba560ff.chunk.js"
+  },
+  {
+    "revision": "7be28177853820fd824d",
+    "url": "/React-Tic-Tac-Toe/static/js/runtime-main.e066fa79.js"
+  }
+]);
